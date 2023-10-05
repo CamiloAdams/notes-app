6be8e3c6e805dffa46b45372c664a4e05a2b3223
Camilo Adames <camiloadames1108@gmail.com>
2023-10-05 00:49:31 -0500

    feat: Add ssl certification

--- notes-api/src/app.js
+++ notes-api/src/app.js
@@ -2,11 +2,13 @@ import express from "express";
 import morgan from "morgan";
 import pkg from "../package.json";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 import { createAdminUser, createRoles } from "./libs/initialSetup";
 import authRoutes from "./routes/auth.routes";
 import notesRoutes from "./routes/notes.routes";
 import userRoutes from "./routes/user.routes";
+import cookiesRoutes from "./routes/cookies.routes";
 
 const app = express();
 
@@ -15,6 +17,7 @@ createRoles().then(() => createAdminUser());
 app.set("pkg", pkg);
 
 app.use(express.json());
+app.use(cookieParser())
 app.use(morgan("dev"));
 app.use(cors());
 
@@ -30,5 +33,6 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", notesRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/cookie", cookiesRoutes);
 
 export default app;
--- notes-api/src/index.js
+++ notes-api/src/index.js
@@ -3,44 +3,44 @@ import fs from "fs";
 import http from "http";
 
 import { swaggerDocs } from "./swagger";
-// import https from "https";
+import https from "https";
 
 import "./database";
 
-// const certificate = fs.readFileSync(
-//     "./cnshelados.tech/certificate.crt",
-//     "utf8",
-//     (err) => {
-//         if (err) console.log(err);
-//     }
-// );
-//
-// const privateKey = fs.readFileSync(
-//     "./cnshelados.tech/private.key",
-//     "utf8",
-//     (err) => {
-//         if (err) console.log(err);
-//     }
-// );
-//
-// const credentials = {
-//     key: privateKey,
-//     cert: certificate,
-// };
-//
-// const httpServer = http.createServer(app);
-// const httpsServer = https.createServer(credentials, app);
+const certificate = fs.readFileSync(
+    "./notesapp.website/certificate.crt",
+    "utf8",
+    (err) => {
+        if (err) console.log(err);
+    }
+);
 
-// httpServer.listen(3000,() =>{
+const privateKey = fs.readFileSync(
+    "./notesapp.website/private.key",
+    "utf8",
+    (err) => {
+        if (err) console.log(err);
+    }
+);
 
-// console.log("Http server listen port 3000");
-// });
-// httpsServer.listen(3001);
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+};
 
-// console.log("Https server listen port 3001");
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 
-app.listen(3000, () => {
-  console.log("Server listen on port:", 3000);
+httpServer.listen(3000,() =>{
 
-  // swaggerDocs(app, 3000);
+console.log("Http server listen port 3000");
 });
+httpsServer.listen(3001);
+
+console.log("Https server listen port 3001");
+
+// app.listen(3000, () => {
+//   console.log("Server listen on port:", 3000);
+
+//   // swaggerDocs(app, 3000);
+// });
